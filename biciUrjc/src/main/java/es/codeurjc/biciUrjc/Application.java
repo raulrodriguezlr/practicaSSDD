8f542970a337ae7e98cbcdd09145cef25b748de5
raulrodriguezlr <12rulo12@gmail.com>
2022-03-23 13:56:58 +0100

    "añadidad linea 9"

--- biciUrjc/src/main/java/es/codeurjc/biciUrjc/Application.java
+++ biciUrjc/src/main/java/es/codeurjc/biciUrjc/Application.java
@@ -6,5 +6,6 @@ public class Application {
     	System.out.println("Programa principal");
     	System.out.println("HOLA");
     	System.out.println("PATI MI COLA xD");
+    	System.out.println("JAJAJ QUE GRACIOSO");
     }
 }
