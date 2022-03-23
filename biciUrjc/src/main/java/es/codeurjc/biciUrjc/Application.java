fa323f006e916570932ee3ef15c05db1ebb839e5
raulrodriguezlr <12rulo12@gmail.com>
2022-03-23 14:02:43 +0100

    Borrado algunos elementos de prueba

--- biciUrjc/src/main/java/es/codeurjc/biciUrjc/Application.java
+++ biciUrjc/src/main/java/es/codeurjc/biciUrjc/Application.java
@@ -4,8 +4,6 @@ public class Application {
 
     public static void main(String[] args) {
     	System.out.println("Programa principal");
-    	System.out.println("HOLA");
-    	System.out.println("PATI MI COLA xD");
-    	System.out.println("JAJAJ QUE GRACIOSO");
+    	
     }
 }
