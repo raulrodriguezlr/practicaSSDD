fde61d098b15461cb0de97986d22128488c5365e
danielrequena <danielrequena0602@gmail.com>
2022-03-23 13:55:35 +0100

    añadido una linea

--- biciUrjc/src/main/java/es/codeurjc/biciUrjc/Application.java
+++ biciUrjc/src/main/java/es/codeurjc/biciUrjc/Application.java
@@ -5,5 +5,6 @@ public class Application {
     public static void main(String[] args) {
     	System.out.println("Programa principal");
     	System.out.println("HOLA");
+    	System.out.println("PATI MI COLA xD");
     }
 }
